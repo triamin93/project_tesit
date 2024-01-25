b3f1bb9a0b4994159044c504707bc68685618231
triamin93 <triaminridhounsada@gmail.com>
2024-01-25 09:23:23 +0700

    fungsi audit sudah

--- operator/coba.php
+++ operator/coba.php
@@ -6,23 +6,15 @@ $conn = mysqli_connect("localhost", "root", "", "testscript");
 
 // Mengambil data session user dan membuat status logout
 $username = $_SESSION["username"];
+$status = "Logout";
+
+echo $username;
 
 // Memanggil query user dan id_user
 $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 $row = mysqli_fetch_assoc($result);
 $id_user = $row["id_user"];
 
-// Membuat status logout
-$status = "Logout";
+echo $id_user;
 
-// Query untuk menyimpan 
-$audit_logout = mysqli_query($conn, "INSERT audit_login (id_user, status) values('$id_user', '$status')");
-
-// Menghapus Seluruh Session
-$_SESSION = [];
-session_unset();
-session_destroy();
-
-// Mengarahkan ke halaman login
-header("location: ../index.php");
-exit;
+?>
